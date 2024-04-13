0d7ecd9b7109fb6677a942e3f24fe82c779c4fa3
Semyon Ishkhanyan <semyon.ishkhanyan@gmail.com>
2024-04-14 01:52:47 +0300

    Initial Commit

---
+++ AppBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppBase
+//
+//  Created by Семён Ишханян on 14.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
