11a0c418fb8228643f0b8235977caf74b8bc804d
Semyon Ishkhanyan <semyon.ishkhanyan@gmail.com>
2024-04-15 18:56:53 +0300

    scenedelegate -> appdelegate | delete storyboard

---
+++ AppBase/Windows/Main/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  AppBase
+//
+//  Created by Семён Ишханян on 15.04.2024.
+//
+
+import UIKit
+
+class MainView: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
