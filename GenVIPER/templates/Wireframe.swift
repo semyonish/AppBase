9a6a8cf5a25f688ea5a1539afcefa29af568f7cf
Semyon Ishkhanyan <semyon.ishkhanyan@gmail.com>
2024-04-28 02:54:51 +0300

    fix gen

--- GenVIPER/templates/Wireframe.swift
+++ GenVIPER/templates/Wireframe.swift
@@ -5,7 +5,7 @@ class {MODULE_NAME}Wireframe {
     static func createModule() -> UIViewController {
         let view = {MODULE_NAME}View()
         let presenter = {MODULE_NAME}Presenter()
-        let router = RegisterRouter()
+        let router = {MODULE_NAME}Router()
         let interactor = {MODULE_NAME}Interactor()
         
         view.presenter = presenter
